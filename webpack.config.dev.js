4c3974301262050bc45eb8844d32ba518e4e3a31
Marcoga <marco.gar7@gmail.com>
2016-04-26 21:45:26 +0200

    added babel-eslint

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -17,25 +17,27 @@ module.exports = {
   plugins: [
     new webpack.HotModuleReplacementPlugin()
   ],
-  resolve: {
-    alias: {
-      'redux-devtools': path.join(__dirname, '..', '..', 'src'),
-      'react': path.join(__dirname, 'node_modules', 'react')
-    }
-  },
-  resolveLoader: {
-    'fallback': path.join(__dirname, 'node_modules')
-  },
   module: {
     loaders: [{
       test: /\.js$/,
       loaders: ['babel'],
       exclude: /node_modules/,
       include: path.join(__dirname, 'src')
+    },  {
+      test: /\.json$/,
+      loaders: ['json']
     }, {
-      test: /\.js$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, '..', '..', 'src')
+      test: /\.css$/,
+      loader: 'style-loader!css-loader'
+    }, {
+      test: /\.(otf|eot|png|svg|ttf|woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      loader: 'url?limit=8192'
+    }, {
+      test: /\.(jpe?g|gif)$/i,
+      loaders: [
+        'file?hash=sha512&digest=hex&name=[hash].[ext]',
+        'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+      ]
     }]
   }
 };
